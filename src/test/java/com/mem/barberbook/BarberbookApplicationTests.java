45b39b8ed9b260b75bae7719417ad86a89df1199
Eliel Mato <elielmato360@gmail.com>
2025-04-30 17:39:41 -0300

    first commit

---
+++ src/test/java/com/mem/barberbook/BarberbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mem.barberbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
